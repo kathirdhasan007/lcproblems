4d09831d319040bbe5327d5b5248cf399ffb6d87
kathirdhasan007 <kathirdhasan007@gmail.com>
2025-07-30 10:05:35 +0530

    merge array

---
+++ mergearray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+      for i in range(m):
+
+
+
+
+
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+m = len(nums1)
+n = len(nums2)
+armerge = Solution()
+armerge.merge(nums1,nums2,m,n)
